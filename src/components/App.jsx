278d3afaebf790f07bb83842fcdf997bc5e189d0
Albert Johanson <alberttruk.11@gmail.com>
2020-12-21 17:43:51 -0600

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/components/App.css';
+
+const App = () => {
+
+
+   
+
+  return (
+<div>
+  <h1>Hola React</h1>
+</div>
+  );
+};
+
+export default App;
